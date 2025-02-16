853b12f3dcb3b57279096abb2dcaaa4c0da13c6f
Revant <86226975+revant7@users.noreply.github.com>
2025-02-16 12:12:45 +0530

    Added Tailwindcss in CRA with specific version 3.4 Successfully.

---
+++ src/components/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SignIn() {
+    return (
+        <div>
+            <form action="" method="post">
+                <label htmlFor="">Enter Name:</label>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorum ratione molestias eveniet aliquid, iusto dignissimos inventore dolor perferendis necessitatibus, debitis neque sequi pariatur sit.
+                </p>
+                <input type="text" name="" id="" />
+            </form>
+        </div>
+    )
+
+}
